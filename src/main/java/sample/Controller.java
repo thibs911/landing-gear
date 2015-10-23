5c4d7d7acf03bf456cfc6f4d0738dc7b6e6f95ad
Clément ACEITUNO <clementaceituno@gmail.com>
2015-10-23 12:00:38 +0200

    reorganisation des ressources + creation de l'interface graphique intiale + controller intial

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,39 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+    @FXML private ImageView gear1;
+    @FXML private ImageView gear2;
+    @FXML private ImageView gear3;
+    @FXML private ImageView lights;
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Image closedDoor = new Image("/img/gear2_retracted.jpg");
+        Image light = new Image("/img/feu_vide.jpg");
+
+        ImageViewSettings(closedDoor,gear1);
+        ImageViewSettings(closedDoor,gear2);
+        ImageViewSettings(closedDoor,gear3);
+        ImageViewSettings(light,lights);
+    }
+
+    private void ImageViewSettings(Image img, ImageView imv)
+    {
+        imv.setImage(img);
+        imv.setFitWidth(200);
+        imv.setPreserveRatio(true);
+        imv.setSmooth(true);
+        imv.setCache(true);
+    }
+
 }
+
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/sample.fxml"));
+
         primaryStage.setTitle("Landing Gear System");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
