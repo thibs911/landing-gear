54ad90b583455814f1c7d92243104ab6070d6ad1
thibs911 <thibs911@hotmail.com>
2015-11-02 16:27:12 +0100

    Mise en place des logback + ajout des différentes libraires closed #5

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,22 +1,20 @@
 package sample;
 
-import javafx.beans.InvalidationListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
-import javafx.geometry.Pos;
 import javafx.geometry.VPos;
-import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-
+import service.DigitalPart;
 
 import java.net.URL;
 import java.util.Observable;
+import java.util.Observer;
 import java.util.ResourceBundle;
 
-public class Controller extends Observable implements Initializable{
+public class Controller implements Initializable, Observer{
     @FXML private ImageView gear1;
     @FXML private ImageView gear2;
     @FXML private ImageView gear3;
@@ -26,7 +24,12 @@ public class Controller extends Observable implements Initializable{
     @FXML private ImageView door3;
     @FXML private GridPane controlGrid;
 
+
+    public DigitalPart digitalPart;
+
     public void initialize(URL location, ResourceBundle resources) {
+
+        this.digitalPart = new DigitalPart();
 
         Image closedDoor = new Image("img/door2_closed.jpg");
         Image closedGear = new Image("/img/gear2_retracted.jpg");
@@ -48,6 +51,10 @@ public class Controller extends Observable implements Initializable{
         controlGrid.setValignment(switchButton, VPos.CENTER);
         controlGrid.setHalignment(switchButton, HPos.CENTER);
 
+        digitalPart.addObserver(this);
+
+
+
     }
 
     private void ImageViewSettings(Image img, ImageView imv)
@@ -60,5 +67,9 @@ public class Controller extends Observable implements Initializable{
     }
 
 
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
 }
 
--- src/main/java/service/DigitalPart.java
+++ src/main/java/service/DigitalPart.java
@@ -1,15 +1,39 @@
 package service;
 
+import com.google.common.collect.Lists;
+import model.LandingSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import thread.LandingThread;
+
+import java.util.List;
+import java.util.Observable;
 
 /**
  * Created by thibs911hotmail.com on 22/10/2015.
  */
-public class DigitalPart{
+public class DigitalPart extends Observable{
 
-private boolean isHandleUp;
+    private static final Logger LOGGER = LoggerFactory.getLogger(DigitalPart.class);
 
-   public void main(){
+    private boolean isHandleUp;
+    private LandingSet landingSetFront, landingSetRight, landingSetLeft;
+    private List<LandingSet> landingSetList;
+
+    public DigitalPart() {
+
+        this.landingSetFront = new LandingSet();
+        this.landingSetLeft = new LandingSet();
+        this.landingSetRight = new LandingSet();
+
+        this.landingSetList = Lists.newArrayList(landingSetFront, landingSetLeft, landingSetRight);
+
+        LOGGER.debug("Création des {} trains d'atterissage {} : ", landingSetList.size(), landingSetList);
+
+
+    }
+
+    public void main(){
      if(!isHandleUp){
          LandingThread landingSet1 = new LandingThread();
          landingSet1.start();
