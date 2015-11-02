0fd316c1359bf25f2fb95bda602d8f1b5fcf21d8
thibs911 <thibs911@hotmail.com>
2015-11-02 16:34:17 +0100

    Modification sur les threads

--- src/main/java/thread/LandingThread.java
+++ src/main/java/thread/LandingThread.java
@@ -1,10 +1,17 @@
 package thread;
 
+import model.LandingSet;
+
 /**
  * Created by thibs911hotmail.com on 27/10/2015.
  */
 public class LandingThread extends Thread {
 
+    private LandingSet landingSet;
+
+    public LandingThread(LandingSet landingSet) {
+        this.landingSet = landingSet;
+    }
 
     public void run(){
 
